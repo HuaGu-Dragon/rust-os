2421cd30dce34e49f1df71e4f98d2c6b9e0a1416
俺不叫化咕龙 <1801943622@qq.com>
2026-03-12 17:05:52 +0800

    make clippy happy

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use core::panic::PanicInfo;
 
 static HELLO: &[u8] = b"Hello World!";
 
+#[allow(clippy::empty_loop)]
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
     let vga_buffer = 0xb8000 as *mut u8;
