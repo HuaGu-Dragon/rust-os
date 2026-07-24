3732b3246fa05592bddd814cdcf82127eaa3f66f
俺不叫化咕龙 <1801943622@qq.com>
2026-07-24 19:24:49 +0800

    support input `0-9` nums

--- src/interrupt.rs
+++ src/interrupt.rs
@@ -1,5 +1,8 @@
 use pic8259::ChainedPics;
-use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use x86_64::{
+    instructions::port::Port,
+    structures::idt::{InterruptDescriptorTable, InterruptStackFrame},
+};
 
 use crate::{
     gdt, print, println,
@@ -21,6 +24,7 @@ static IDT: LazyLock<InterruptDescriptorTable> = LazyLock::new(|| {
             .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
 
         idt[InterruptIndex::Timer as u8].set_handler_fn(timer_interrupt_handler);
+        idt[InterruptIndex::Keyboard as u8].set_handler_fn(keyboard_interrupt_handler);
     };
 
     idt
@@ -30,6 +34,7 @@ static IDT: LazyLock<InterruptDescriptorTable> = LazyLock::new(|| {
 #[repr(u8)]
 pub enum InterruptIndex {
     Timer = PIC_1_OFFSET,
+    Keyboard,
 }
 
 pub fn init_idt() {
@@ -49,6 +54,32 @@ extern "x86-interrupt" fn double_fault_handler(
 
 extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFrame) {
     print!(".");
+
+    unsafe {
+        PICS.lock()
+            .notify_end_of_interrupt(InterruptIndex::Timer as u8)
+    };
+}
+
+extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStackFrame) {
+    let mut port = Port::new(0x60);
+    let code: u8 = unsafe { port.read() };
+    let key = match code {
+        0x02 => Some('1'),
+        0x03 => Some('2'),
+        0x04 => Some('3'),
+        0x05 => Some('4'),
+        0x06 => Some('5'),
+        0x07 => Some('6'),
+        0x08 => Some('7'),
+        0x09 => Some('8'),
+        0x0a => Some('9'),
+        0x0b => Some('0'),
+        _ => None,
+    };
+    if let Some(key) = key {
+        print!("{}", key);
+    }
 
     unsafe {
         PICS.lock()
